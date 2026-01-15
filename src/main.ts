12341cb95f2c213aa49d5914bbc512520fe18ee9
Sara Salzano <sarasalzano95@gmail.com>
2026-01-15 13:39:49 +0100

    crea funzione get actress e type guard isactress

--- src/main.ts
+++ src/main.ts
@@ -28,7 +28,81 @@ type Person = {
 // Le nazionalità accettate sono: American, British, Australian, Israeli-American, South African, French, Indian, Israeli, Spanish, South Korean, Chinese.
 
 type Actress = Person & {
-most_famous_movies: [string, string, string];
-awards: string;
-nationality: "American" | "British" | "Australian" | "Israeli-American" | "South African" | "French" | "Indian" | "Israeli" | "Spanish" | "South Korean"| "Chinese" 
+  most_famous_movies: [string, string, string];
+  awards: string;
+  nationality:
+    | "American"
+    | "British"
+    | "Australian"
+    | "Israeli-American"
+    | "South African"
+    | "French"
+    | "Indian"
+    | "Israeli"
+    | "Spanish"
+    | "South Korean"
+    | "Chinese";
 };
+
+// Milestone 3
+// Crea una funzione getActress che, dato un id, effettua una chiamata a:
+
+// GET /actresses/:id
+// La funzione deve restituire l’oggetto Actress, se esiste, oppure null se non trovato.
+
+// Utilizza un type guard chiamato isActress per assicurarti che la struttura del dato ricevuto sia corretta.
+
+function isActress(data: unknown): data is Actress {
+  if (
+    data &&
+    typeof data === "object" &&
+    "id" in data &&
+    typeof data.id === "number" &&
+    "name" in data &&
+    typeof data.name === "string" &&
+    "birth_year" in data &&
+    typeof data.birth_year === "number" &&
+    "death_year" in data &&
+    typeof data.death_year === "number" &&
+    "biography" in data &&
+    typeof data.biography === "string" &&
+    "image" in data &&
+    typeof data.image === "string" &&
+    "most_famous_movies" in data &&
+    //controlla se most famous movies è un array
+    Array.isArray(data.most_famous_movies) &&
+    data.most_famous_movies.length === 3 &&
+    data.most_famous_movies.every(a => typeof a === "string") &&
+    "awards" in data &&
+    typeof data.awards === "string" &&
+    "nationality" in data &&
+    typeof data.nationality === "string"
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+async function getActress(id: number): Promise<Actress | null> {
+  try {
+    const response = await fetch(`http://localhost:3333/actresses/${id}`);
+    if (!response.ok) {
+      throw new Error("Errore HTTP");
+    }
+    const data: unknown = await response.json();
+    if (!isActress(data)) {
+      throw new Error("I dati sono in un formato non valido");
+    } else {
+      return data;
+    }
+    //error è unknown quindi faccio un check
+  } catch (error) {
+    if(error instanceof Error){
+      console.log("Errore recupero attrice")
+    } else {
+    console.error(error);
+    return null;
+    }
+  }
+}
